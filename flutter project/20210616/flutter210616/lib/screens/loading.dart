5fbca36a195e588721e5c93d728a8a36031a806a
enterblack <gkswnsgml_@naver.com>
2021-06-16 23:03:39 +0900

    Json setting

--- flutter project/20210616/flutter210616/lib/screens/loading.dart
+++ flutter project/20210616/flutter210616/lib/screens/loading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 class Loading extends StatefulWidget {
 
   @override
@@ -9,20 +10,45 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    super.initState();
+    getLocation();
+    fetchData();
+  }
   void getLocation() async{
 
-    Position position = await Geolocator.
-    getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    print(position);
+    try {
+      Position position = await Geolocator.
+      getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      print(position);
+    }catch(e){
+      print("There was a problem with the internet connection.");
+    }
   }
+  
+  void fetchData() async{
+    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1');
+    if(response.statusCode == 200){
+      String jsonData = response.body;
+      var myJson = jsonDecode(jsonData)['weather'][0]['description'];
+      print(myJson);
+      var myJson1 = jsonDecode(jsonData)['wind']['speed'];
+      print(myJson1);
+      var myJson2 = jsonDecode(jsonData)['id'];
+      print(myJson2);
+    }
+
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
-          onPressed: (){
-            getLocation();
-          },
+          onPressed: null,
           child: Text("Get my location",
             style: TextStyle(
               color: Colors.white
