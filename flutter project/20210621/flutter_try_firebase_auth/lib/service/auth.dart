f1ae0130532415e7bd6d59df0c94c1477599be85
enterblack <gkswnsgml_@naver.com>
2021-06-23 19:37:28 +0900

    anonymos login, signOut

---
+++ flutter project/20210621/flutter_try_firebase_auth/lib/service/auth.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_try_firebase_auth/models/user.dart';
+import 'package:flutter_try_firebase_auth/service/auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //create user obj based on FireBaseUser
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  // auth Change user stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged
+        //.map((FirebaseUser user) => _userFromFirebaseUser(user));
+        .map(_userFromFirebaseUser);
+  }
+
+  //sign in anon
+  Future signInAnon() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  //sign in with email & password
+
+  //reigister with email & password
+
+  //sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
